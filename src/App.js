876702273bbb4737781682b12822b02ea1cee287
Harshana369 <harsha36998@gmail.com>
2022-08-17 19:35:47 +0530

    link attribute used

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
 import About from "./pages/About";
 import Products from "./pages/Products";
-
+import Error from "./pages/Error";
 function App() {
   return (
     <BrowserRouter>
+    <nav>our navbar</nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error/>}/>
       </Routes>
+      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
