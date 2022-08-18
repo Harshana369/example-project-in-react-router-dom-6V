2e062711890d14f7391231bf10872e4b6584f43e
Harshana369 <harsha36998@gmail.com>
2022-08-18 13:15:42 +0530

    Complte project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Error from "./pages/Error";
-import SharedLayout from './pages/SharedLayout'
-import SingleProduct from './pages/SingleProduct'
-import React, { useState } from 'react'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import SharedLayout from "./pages/SharedLayout";
+import SingleProduct from "./pages/SingleProduct";
+import React, { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import SharedProductLayout from './pages/SharedProductLayout'
 const App = () => {
-  const [user,setUser] = useState(null);
+  const [user, setUser] = useState(null);
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
-        <Route path="products/:productId" element={<SingleProduct />} />
+
+        <Route path='products' element={<SharedProductLayout/>}>
+          <Route index element={<Products />} />
+          <Route path=":productId" element={<SingleProduct />} />
+        </Route>
+
         <Route path="login" element={<Login setUser={setUser} />} />
         <Route
           path="dashboard"
@@ -31,6 +36,6 @@ const App = () => {
       </Route>
     </Routes>
   );
-}
+};
 
 export default App;
--- src/pages/SharedProductLayout.js
+++ src/pages/SharedProductLayout.js
@@ -1,9 +1,8 @@
 import { Outlet } from "react-router-dom";
-import StyledNavbar from "../components/StyledNavbar";
 const Home = () => {
   return (
     <>
-      <StyledNavbar />
+      <h2>products</h2>
       <Outlet />
     </>
   );
