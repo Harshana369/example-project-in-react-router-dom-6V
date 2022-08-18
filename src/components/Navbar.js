0af6445e8a4eab5e973018f327e923ae2f1b5080
Harshana369 <harsha36998@gmail.com>
2022-08-18 08:21:16 +0530

    NavLink (style)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/products">Products</Link>
+      <NavLink
+        to="/"
+        style={({ isActive }) => {
+          return { color: isActive ? "red" : "grey" };
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/products">Products</NavLink>
     </nav>
   );
 };
